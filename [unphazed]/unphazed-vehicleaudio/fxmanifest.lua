327d5d5351c1dc7518264c123665fe0f1a11d2ae
Brandon <lockedaxev3@outlook.com>
2026-03-08 17:55:24 -0500

    Added Vehicles Back Part 4

---
+++ [unphazed]/unphazed-vehicleaudio/fxmanifest.lua
@@ -0,0 +1,52 @@
+fx_version 'cerulean'
+author 'Brandon'
+version '2.0.0'
+game 'gta5'
+
+files {
+	"audioconfig/*.dat151.rel",
+	"audioconfig/*.dat54.rel",
+	"audioconfig/*.dat10.rel",
+	"sfx/**/*.awc"
+}
+
+------------ Replace ------------
+-- data_file 'AUDIO_GAMEDATA' 'audioconfig/blank_game.dat'
+-- data_file 'AUDIO_SOUNDDATA' 'audioconfig/blank_sounds.dat'
+-- data_file "AUDIO_SYNTHDATA" "audioconfig/blank_amp.dat"
+-- data_file 'AUDIO_WAVEPACK' 'sfx/dlc_blank'
+
+------------ Vamosv8 ------------
+data_file 'AUDIO_GAMEDATA' 'audioconfig/vamosv8_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/vamosv8_sounds.dat'
+-- data_file "AUDIO_SYNTHDATA" "audioconfig/vamosv8_amp.dat" -- Missing?
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_vamosv8'
+
+------------ Picadorexr ------------
+data_file 'AUDIO_GAMEDATA' 'audioconfig/picadorexr_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/picadorexr_sounds.dat'
+-- data_file "AUDIO_SYNTHDATA" "audioconfig/picadorexr_amp.dat" -- Missing?
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_picadorexr'
+
+------------ Monroec ------------
+data_file 'AUDIO_GAMEDATA' 'audioconfig/monroec_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/monroec_sounds.dat'
+data_file "AUDIO_SYNTHDATA" "audioconfig/monroec_amp.dat"
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_monroec'
+
+------------ Shinobid ------------
+data_file 'AUDIO_GAMEDATA' 'audioconfig/shinobid_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/shinobid_sounds.dat'
+-- data_file "AUDIO_SYNTHDATA" "audioconfig/shinobid_amp.dat" -- Missing?
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_shinobid'
+
+------------ Stratumc ------------
+data_file 'AUDIO_GAMEDATA' 'audioconfig/stratumc_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/stratumc_sounds.dat'
+data_file "AUDIO_SYNTHDATA" "audioconfig/stratumc_amp.dat"
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_zircoflow'
+
+------------ rrfr60x ------------
+data_file 'AUDIO_GAMEDATA' 'audioconfig/aq14nisvq37vhrt_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/aq14nisvq37vhrt_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_aq14nisvq37vhrt'
---
+++ [unphazed]/unphazed-wheels/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+author 'Logan & Brandon'
+
+files {
+  'data/**/*.meta',
+}
+  
+escrow_ignore {
+  'data/*',
+}
+
+data_file 'CARCOLS_FILE' 'data/**/*carcols.meta'
+
+
+dependency '/assetpacks'
